8aaa1931a22449e93b5bca087b463664deb4520f
Mexi <100799507+MexiMoo@users.noreply.github.com>
2024-10-16 11:07:15 +0100

    PLC functies en lamp codes nagekeken en getest

--- Proftaak Arduino/proftaak.c
+++ Proftaak Arduino/proftaak.c
@@ -37,21 +37,24 @@ void SetSyncLed(bool ledOn) //af
 }
 bool IsSyncBitSet() //af
 {
-	//uint8_t syncBit = ~PINB | (1 << 4);
-	uint8_t test = PINB & 8;
+	//uint8_t syncBit = ~PINB | (1 << 8);
+	uint8_t syncBit = ~PINB & 8;
 	
-	if (!test == 0)
+	if (syncBit == 0)
 	{
 		return false;
 	}
 	else
 	{
 		return true;
+		PORTD = 0xFF;
+		delayms(1000);
 	}
 }
 bool IsSyncDetected() //af //Wachten op opgaande flank (rising edge)
 {
-	uint8_t sync = PINB & 8;
+	//uint8_t sync = PINB & 8;
+	uint8_t syncBit = (PINB & 0x08) >> 3; //Masker voor bit 3 (1000) en verschuif naar bit 0
 	while (sync == 1)
 	{
 		
@@ -66,50 +69,68 @@ bool IsSyncDetected() //af //Wachten op opgaande flank (rising edge)
 bool WaitUntilSyncReleased()
 {
 	bool isSyncPresent = false;
-	while (isSyncPresent = false)
+	while (isSyncPresent == false)
 	{
 		isSyncPresent = IsSyncBitSet();
 	}
-	return true;
+	return isSyncPresent;
 }
-void ControlLamps (uint8_t functionCode)
+void ControlLamps(uint8_t functionCode) //getest
 {
-	if (functionCode > 7)
+	if (functionCode == 0)
 	{
-		// Buiten de 0-7, er gebeurt niks met de lampen
-		return;
+		// Actie voor functiecode 000 (0)
 	}
-	
-	if (PINB & 8)
+	else if (functionCode == 1)
 	{
-		PORTD = 0x01;
+		// Actie voor functiecode 001 (1)
 	}
-	else if (PINB & 9)
+	else if (functionCode == 2)
 	{
-		PORTD = 0x02;
+		// Actie voor functiecode 010 (2)
 	}
-	else if(PINB & 10)
+	else if (functionCode == 3)
 	{
-		PORTD = 0x04;
+		// Actie voor functiecode 011 (3)
+	}
+	else if (functionCode == 4)
+	{
+		// Actie voor functiecode 100 (4)
+	}
+	else if (functionCode == 5)
+	{
+		// Actie voor functiecode 101 (5)
+	}
+	else if (functionCode == 6)
+	{
+		// Actie voor functiecode 110 (6)
+	}
+	else if (functionCode == 7)
+	{
+		// Actie voor functiecode 111 (7)
+	}
+	else
+	{
+		// Doe niets als de functiecode niet tussen 0 en 7 ligt
 	}
 }
-uint8_t GetPLCData() // Masker bits 2-0 & Masker bit 3 en verschuif naar bit 0 positie
+uint8_t GetPLCData(void) //getest
 {
-	uint8_t bit210 = PINB & ;
-	uint8_t bit3 = PINB & 8;
-	return (bit210 | (bit3 << 3));
+	uint8_t data = PINB & 0x0F; //Masker voor bits 0, 1, 2 en 3 (1111)
+	return data;
 }
-uint8_t GetFunctionCode(uint8_t inputData)
+uint8_t GetFunctionCode(uint8_t inputData) //getest
 {
-	return inputData & 0x07;
+	uint8_t functionCode = inputData & 0x07; //Masker voor bits 0, 1 en 2 (0111)
+	return functionCode;
 }
 void SetAcknowledge() // Zet bit 7 (ACK-bit) op "1", zonder de andere bits in PORTD te wijzigen
 {
-	PORTD |= (1 << 7);
+	//PORTD |= (1 << 7);
 }
 void ClearAcknowledge() // Zet bit 7 (ACK-bit) op "0", zonder de andere bits in PORTD te wijzigen
 {
-	PORTD |= (0 << 7);
+	//PORTD |= (0 << 7);
 }
 ///////////////////////////////////////////////////////////////////////////////
 // program entry point
@@ -126,12 +147,17 @@ int main(void)
 		//uint8_t button_status = PINB & 0x07;  // Lees de status van drukknoppen D2..D0 (bits 2..0 van poort B)
 		//ControlLamps(button_status);
 		
-		while (IsSyncBitSet() == true)
-		{
-			SetSyncLed(true);
-			delayms(100);
-			SetSyncLed(false);
-			delayms(100);
-		}
+		uint8_t plcData = GetPLCData();
+		uint8_t functionCode = GetFunctionCode(plcData);
+		ControlLamps(functionCode);
+		
+		
+		//while (WaitUntilSyncReleased() == true)
+		//{
+			//SetSyncLed(true);
+			//delayms(100);
+			//SetSyncLed(false);
+			//delayms(100);
+		//}
 	}
 }
